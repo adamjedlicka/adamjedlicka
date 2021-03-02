50ed5c36b8748dd0f8967c4e34a2618854601b88
Adam Jedlička <adajedlicka@gmail.com>
2021-03-02 22:15:10 +0100

    Another commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
+
     <footer className="text-gray-700 body-font">
       <div className="container max-w-3xl px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <p className="text-sm text-gray-500 sm:py-2 sm:mt-0 mt-4">
